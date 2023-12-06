fe231cdc91bb7fe1d3bbcc3a92ada5f55416e28a
Nguyễn Thiện Trung <trungnguyen@vinasource.com>
2023-12-06 15:40:27 +0000

    dđ

--- install-gamos-6.2.0.sh
+++ install-gamos-6.2.0.sh
@@ -25,8 +25,8 @@ sudo apt-get -y install libxpm-dev
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-numpy
 sudo apt-get -y install git
-mkdir "~/gamos620" # Create gamos620 dir at home
+mkdir ~/gamos620 # Create gamos620 dir at home
 wget http://fismed.ciemat.es/GAMOS/download/GAMOS.6.2.0/download_scripts.sh
 bash ./download_scripts.sh
 cd scripts
-bash ./installGamos.sh "~/gamos620"
+bash ./installGamos.sh ~/gamos620
