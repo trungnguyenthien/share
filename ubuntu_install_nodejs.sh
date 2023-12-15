dfef8df521c4367dc9b97af05dc26fc03589bd41
Nguyễn Thiện Trung <trungnguyen@vinasource.com>
2023-12-16 06:16:25 +0700

    Add files via upload

---
+++ ubuntu_install_nodejs.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_14.x $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_14.x $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install nodejs
+node -v
+npm -v
